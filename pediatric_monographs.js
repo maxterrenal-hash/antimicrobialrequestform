832c4aab9ec2462050647e845734cfc80a35c424
maxterrenal-hash <maxterrenal@gmail.com>
2025-11-15 03:24:54 +0800

    Create pediatric_monographs.js

---
+++ pediatric_monographs.js
@@ -0,0 +1,362 @@
+// pediatric_monographs.js
+// Skeleton monographs for all PEDIATRIC drugs.
+// Option B: all have weightBased + mgPerKgDose (set to 0 by default).
+// Calculator will appear only once you put mgPerKgDose > 0.
+
+window.PEDIATRIC_MONOGRAPHS = {
+  // Pediatric restricted
+  "Acyclovir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Amphotericin B": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Aztreonam": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Caspofungin (Echinocandin)": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Cefepime": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Colistin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Ciprofloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Doripenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Ertapenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Famciclovir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Flucytosine": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Imipenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Levofloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Meropenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Micafungin (Echinocandin)": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Moxifloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Ofloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Remdesivir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Valganciclovir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Vancomycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Voriconazole": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+
+  // Pediatric monitored
+  "Amikacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Cefotaxime": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Ceftriaxone": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Ceftazidime": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Clindamycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Gentamicin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Kanamycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Neomycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Piperacillin–Tazobactam": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Streptomycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  },
+  "Tobramycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: "",
+    weightBased: true,
+    mgPerKgDose: 0
+  }
+};
