069160e6a3cc8bb65584978c6cfee4b76b32e958
maxterrenal-hash <maxterrenal@gmail.com>
2025-11-15 03:24:21 +0800

    Create adult_monographs.js

---
+++ adult_monographs.js
@@ -0,0 +1,269 @@
+// adult_monographs.js
+// Skeleton monographs for all ADULT drugs.
+// Fill these fields later from Sanford / PPS / local guidelines.
+
+window.ADULT_MONOGRAPHS = {
+  "Acyclovir IV": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Amikacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Amphotericin B": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Aztreonam": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Cefepime": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ceftazidime–Avibactam (NF)": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ceftolozane–Tazobactam (NF)": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Colistin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Doripenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ertapenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ganciclovir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Gentamicin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Imipenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Linezolid": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Meropenem": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Micafungin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Polymyxin B": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Remdesivir": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Tigecycline": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Valganciclovir oral": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Vancomycin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Voriconazole": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+
+  // Adult monitored
+  "Ceftriaxone": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ceftazidime": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Ciprofloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Fluconazole": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Levofloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Moxifloxacin": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  },
+  "Piperacillin–Tazobactam": {
+    spectrum: "",
+    dosing: "",
+    renal: "",
+    duration: "",
+    monitoring: "",
+    warnings: "",
+    ams: ""
+  }
+};
